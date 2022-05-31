8d5e93f2ef429d9e050902da99c7624c57cb6481
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-05-31 09:53:15 +0900

    a

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow("LE2B_02_アスマ_ショウタ_AL3");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
