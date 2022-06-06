8f5cc199e8102351f5b840bca2d6c66ebf326172
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-06-06 23:10:32 +0900

    a

--- DirectXGame/Transform.cpp
+++ DirectXGame/Transform.cpp
@@ -54,3 +54,60 @@ void worldTransformRole(WorldTransform *worldTransform_, float x, float y, float
 	matRot *= matRotY;
 	worldTransform_->matWorld_ *= matRot;
 }
+
+void matScaleSet(WorldTransform* worldTransform_, float x, float y, float z)
+{
+	worldTransform_->matWorld_.m[0][0] *= x;
+	worldTransform_->matWorld_.m[1][1] *= y;
+	worldTransform_->matWorld_.m[2][2] *= z;
+}
+
+void matTransrationSet(WorldTransform* worldTransform_, float x, float y, float z)
+{
+	worldTransform_->matWorld_.m[3][0] *= x;
+	worldTransform_->matWorld_.m[3][1] *= y;
+	worldTransform_->matWorld_.m[3][2] *= z;
+}
+
+void matRoleSet(WorldTransform* worldTransform_, float x, float y, float z)
+{
+	Matrix4 matRot;
+	Matrix4 matRotX(
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, cos(x), sin(x), 0.0f,
+		0.0f, -sin(x), cos(x), 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	);
+	Matrix4 matRotY(
+		cos(y), 0.0f, -sin(y), 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		sin(y), 0.0f, cos(y), 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	);
+	Matrix4 matRotZ(
+		cos(z), sin(z), 0.0f, 0.0f,
+		-sin(z), cos(z), 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	);
+
+	matRot = matRotZ;
+	matRot *= matRotX;
+	matRot *= matRotY;
+	worldTransform_->matWorld_ *= matRot;
+
+	/*worldTransform_->matWorld_.m[0][0] *= cos(y) * cos(z);
+	worldTransform_->matWorld_.m[0][1] *= -sin(z);
+	worldTransform_->matWorld_.m[0][2] *= sin(y);
+	worldTransform_->matWorld_.m[1][0] *= sin(z);
+	worldTransform_->matWorld_.m[1][1] *= cos(x) * cos(z);
+	worldTransform_->matWorld_.m[1][2] *= -sin(x);
+	worldTransform_->matWorld_.m[2][0] *= -sin(y);
+	worldTransform_->matWorld_.m[2][1] *= sin(x);
+	worldTransform_->matWorld_.m[2][2] *= cos(x) * cos(y);*/
+
+	/*worldTransform_->matWorld_.m[1][1] = cos(x);
+	worldTransform_->matWorld_.m[1][2] = -sin(x);
+	worldTransform_->matWorld_.m[2][1] = sin(x);
+	worldTransform_->matWorld_.m[2][2] = cos(x);*/
+}
--- DirectXGame/Transform.h
+++ DirectXGame/Transform.h
@@ -11,3 +11,8 @@ void worldTransformMove(WorldTransform *worldTransform_, float x, float y, float
 
 //WorldTransform‚Ì‰ñ“]‚ÌŠÖ”
 void worldTransformRole(WorldTransform *worldTransform_, float x, float y, float z);
+
+
+void matScaleSet(WorldTransform* worldTransform_, float x, float y, float z);
+void matTransrationSet(WorldTransform* worldTransform_, float x, float y, float z);
+void matRoleSet(WorldTransform* worldTransform_, float x, float y, float z);
